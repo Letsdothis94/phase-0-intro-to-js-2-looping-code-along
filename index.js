5231089f182d472d9092289e3e5930c3fd29cb40
Cristian Contreras <contreras.cristian994@gmail.com>
2022-11-06 15:54:21 -0500

    commit tree 7a3f75f4f4996a80bda4d3bb1e1930d454b488d4

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+const name = [ "Guadalupe", "Ollie", "Aki"];
+const messages = [];
+function writeCards(name) {
+    for (let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+        
+    }
+
+    return name;
+}
+
+writeCards();
